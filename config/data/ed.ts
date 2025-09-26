ac35a0f00e3905b4843faab4fb7c3c09f1535583
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-09-26 22:45:05 +0530

    move education stuff to a separate file

---
+++ config/data/ed.ts
@@ -0,0 +1,33 @@
+export interface educationItem {
+    graduatedIn: String;
+    title: String;
+    body: String;
+    school: String;
+    location: String;
+}
+
+export const education: educationItem[] = [
+    {
+        graduatedIn: "Batch of 2018",
+        school: "Delhi Public School",
+        title: "Secondary School, DPS (10th Grade)",
+        body: "Aggregate of 94% in AISSE 2018",
+        location: "Vidyut Nagar, Dadri"
+    },
+    {
+        graduatedIn: "Batch of 2020",
+        school: "Delhi Public School, Vidyut Nagar, Dadri",
+        title: "Senior Secondary School, DPS (12th Grade)",
+        body: "Aggregate of 91% in AISSCE 2020",
+        location: "Vidyut Nagar, Dadri"
+
+    },
+    {
+        graduatedIn: "Batch of 2024",
+        school: "S.R.M Institute of Science and Technology",
+        title: "Bachelor's of Technology, SRMIST",
+        body: "Computer Science and Engineering; 8.46 CGPA",
+        location: ", kattankulathur, Chennai Tamil Nadu"
+
+    },
+]
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,6 +1,11 @@
-import { Bitter } from "next/font/google";
+import { Bitter, Nunito } from "next/font/google";
 
 export const bitter = Bitter({
   weight: ["400", "600", "700", "800", "900"],
   preload: false,
 })
+
+export const nunito = Nunito({
+  weight: ["400", "600", "700", "800", "900"],
+  preload: false,
+});

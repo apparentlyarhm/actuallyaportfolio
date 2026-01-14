519d2a328978a806f54caf25f06e0df08003f282
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-01-14 12:28:07 +0530

    use globals for font

--- config/fonts.ts
+++ config/fonts.ts
@@ -2,15 +2,18 @@ import { Bitter, Nunito, JetBrains_Mono } from "next/font/google";
 
 export const bitter = Bitter({
   weight: ["400", "600", "700", "800", "900"],
-  preload: false,
+  subsets: ["latin"],
+  variable: "--font-serif",
 })
 
 export const nunito = Nunito({
   weight: ["400", "600", "700", "800", "900"],
-  preload: false,
-});
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const jbMono = JetBrains_Mono({
   weight: ["400", "600", "700", "800"],
-  preload: false,
+  subsets: ["latin"],
+  variable: "--font-mono",
 })

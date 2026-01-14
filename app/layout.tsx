a7c46309ca0abc1d2aa7006398da66a262fcc8d6
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2026-01-14 12:35:17 +0530

    update metadata + use font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,21 @@ import { Metadata, Viewport } from "next";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
+import { bitter, jbMono, nunito } from "@/config/fonts";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+  title: "Arhum's Portfolio",
+  description: "online resume and various links I have to offer",
+  keywords: ["Software Engineer"],
+  authors: [{ name: "Arhum", url: "https://arhm.dev" }],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://arhm.dev",
+    title: "Arhum | YET ANOTHER PORTFOLIO",
+    description: "online resume and various links I have to offer",
+    siteName: "Arhum's Portfolio",
   },
-  description: siteConfig.description,
   icons: {
     icon: "/favicon.ico",
   },
@@ -32,7 +39,12 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en">
       <head />
       <body
-        className="min-h-screen text-foreground bg-background antialiased"
+        className={`min-h-screen text-foreground bg-background antialiased
+            ${nunito.variable}
+            ${bitter.variable}
+            ${jbMono.variable}
+          `
+        }
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
